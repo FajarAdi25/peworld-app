4ae6795b92c30b1c7e7158756d506669e89a26f4
Fajar Adi Prasetio <69068262+FajarAdi25@users.noreply.github.com>
2024-02-12 11:55:00 +0700

    Update index.js

--- components/module/HireSection/index.js
+++ components/module/HireSection/index.js
@@ -11,7 +11,7 @@ const HireSection = () => {
           yang siap untuk bergabung dengan tim Anda.
         </p>
 
-        <form onSubmit={handleHire} className="mt-10">
+        <form onSubmit="" className="mt-10">
           <div>
             <label className="block text-sm font-medium leading-6 text-gray-400">
               Tujuan tentang pesan ini
@@ -20,7 +20,7 @@ const HireSection = () => {
             <input
               name="objective"
               type="text"
-              onChange={handleChange}
+              onChange=""
               required
               placeholder="Projek"
               className="block w-full rounded-md border-0 px-3 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
